03a7cdc2a6ce88c335df246f6770223d307819ed
Nik Nichols <nik@vizionikmedia.com>
2026-06-21 14:51:22 -0500

    feat: import and expose all modules in package __init__

--- src/my_automated_traffic/__init__.py
+++ src/my_automated_traffic/__init__.py
@@ -1,1 +1,20 @@
+"""My Automated Traffic package initializer."""
 
+from .blog_agent import BlogAgent
+from .bridge_page import QuizPageGenerator
+from .cli import main_cli, add_offer
+from .database import DatabaseManager
+from .main import main
+from .social_agent import SocialAgent
+from .video_agent import VideoAgent
+
+__all__ = [
+    "add_offer",
+    "BlogAgent",
+    "DatabaseManager",
+    "main",
+    "main_cli",
+    "QuizPageGenerator",
+    "SocialAgent",
+    "VideoAgent",
+]
